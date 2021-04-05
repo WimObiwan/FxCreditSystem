eb8d599e59feef5cae1425189b2d5a90e30011ec
WimObiwan <wim@obiwan.be>
2021-04-05 20:43:18 +0200

    Added missing service registrations.

--- FxCreditSystem.API/ServiceCollectionExtensions.cs
+++ FxCreditSystem.API/ServiceCollectionExtensions.cs
@@ -15,14 +15,16 @@ namespace FxCreditSystem.API
 
         public static IServiceCollection AddFxCreditSystemCore(this IServiceCollection services)
         {
-            services.AddTransient<Common.IUserQueryHandler, Core.UserQueryHandler>();
             services.AddTransient<Core.IAuthorizationService, Core.AuthorizationService>();
+            services.AddTransient<Common.IUserQueryHandler, Core.UserQueryHandler>();
+            services.AddTransient<Common.IAccountQueryHandler, Core.AccountQueryHandler>();
             return services;
         }
 
         public static IServiceCollection AddFxCreditSystemRepository(this IServiceCollection services)
         {
             services.AddTransient<Common.IUserRepository, Repository.UserRepository>();
+            services.AddTransient<Common.IAccountRepository, Repository.AccountRepository>();
             return services;
         }
     }
